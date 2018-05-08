bfbf3772436cd4ff4e69f5cac3a1d20d1bdb470f
traviskeri <traviskeri@csus.edu>
2018-05-08 13:01:52 -0700

    smtp

--- smtp_test.py
+++ smtp_test.py
@@ -51,7 +51,7 @@ if recv2[:3] != '354':
 
 #Send message data.
 print("Sending msg Command")
-clientSocket.send(msg)
+clientSocket.send("\r\n SMTP Mail Client Test\nI love computer networkin!\r\n.\r\n")
 recv2 = clientSocket.recv(1024)
 print(recv2)
 if recv2[:3] != '250':
@@ -59,12 +59,12 @@ if recv2[:3] != '250':
 
 
 #Message ends with a single period.
-print("Sending endmsg Command")
-clientSocket.send(endmsg)
-recv2 = clientSocket.recv(1024)
-print(recv2)
-if recv2[:3] != '250':
-	print('250 reply not received from sever.')
+#print("Sending endmsg Command")
+#clientSocket.send(endmsg)
+#recv2 = clientSocket.recv(1024)
+#print(recv2)
+#if recv2[:3] != '250':
+#	print('250 reply not received from sever.')
 
 
 # Send QUIT command and get server response.
